f30e0c627c6d9f94bdda1e14f5f8c9a5a3c28caa
Alirezrz <alirezatu24@gmail.com>
2026-06-20 13:18:15 +0330

    chore: add project template

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// TODO: include your own headers here
+
+/**
+ * Banking System — Phase 1
+ */
+
+
+int main() {
+    // TODO: initialize Bank object and load data from JSON
+
+ 
+    // TODO: save data before exit
+    return 0;
+}
