ab43b03d41c282713c98c790fab7578cb3ba4b66
Armin Sahraei <arminsahraei7@gmail.com>
2026-07-21 22:44:11 +0330

    online payment OTP checks added

--- src/user.cpp
+++ src/user.cpp
@@ -21,6 +21,8 @@ typedef long double ld;
 #define mp make_pair
 #define all(x) (x).begin(), (x).end()
 
+const double maximum_transaction = 10000000;
+
 
 string GetTime(){
     time_t now = time(0);
@@ -390,7 +392,7 @@ int main(){
     string cmd;
     int User_idx = -1;
     int OTP;
-    auto start_time = chrono::high_resolution_clock::now();
+    auto OTP_start_time = chrono::high_resolution_clock::now();
     while(cin >> cmd){
         payload.clear();
         if(cmd == "EOF"){
@@ -806,6 +808,42 @@ int main(){
             cout << "OTP: " << OTP << '\n';
             cout << "expires in 120 seconds\n";
         }
+        else if (cmd == "online_payment"){
+            double amount;
+            string from_account, to_account;
+            cin >> from_account >> to_account;
+            cin >> amount;
+            vector <string> Accs = Ucore.AccList(User_idx);
+            bool belong_to = false;
+            for (auto& name : Accs){
+                if (name == from_account){
+                    belong_to = true;
+                }
+            }
+            if (!belong_to){
+                cout << "Error: Account does not belong to user." << endl;
+                continue;
+            }
+            cout << "Enter OTP:\n";
+            int entered_OTP;
+            cin >> entered_OTP;
+            if (entered_OTP != OTP){
+                cout << "Error: Invalid OTP.\n";
+                continue;
+            }
+            auto now_time = chrono::high_resolution_clock::now();
+            double time_since_OTP_request = chrono::duration<double>(now_time - OTP_start_time);
+            if (time_since_OTP_request > 120){
+                cout << "Error: OTP expired.\n";
+                continue;
+            }
+            if (amount > maximum_transaction){
+                cout << "Error: transaction limit exceeded.\n";
+                continue;
+            }
+            // TODO runAdmin
+        }
+        
         else {
             cout << "Error: Unknown command" << endl;
         }
