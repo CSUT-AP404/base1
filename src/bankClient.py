13588f032dd6b03b48fe10f36fd70bb3d74fb281
Mohammadi-Matin <Matin100@yahoo.com>
2026-08-12 11:08:18 +0330

    "update : Client menu added (Done)"

--- src/bankClient.py
+++ src/bankClient.py
@@ -1,13 +1,11 @@
 import requests
-
-
 class AdminClient:
     def __init__(self, baseLink: str = "http://127.0.0.1:47001"):
         self.baseLink = baseLink
         self.token = None
 
     def _request(self, method: str, endpoint: str, jsonData: dict = None, params: dict = None) -> dict:
-        Link = f"{self.baseLink}{endpoint}"
+        link  = f"{self.baseLink}{endpoint}"
         headers = {"Content-Type": "application/json"}
 
         if self.token:
@@ -16,11 +14,11 @@ class AdminClient:
         try:
             response = requests.request(
                 method=method.upper(),
-                url=Link,
+                url=link,
                 headers=headers,
                 json=jsonData,
                 params=params,
-                timeout=10
+                timeout=15 #این توی خود کتابخونه ریکوست هستش -----> اگه به مشکل بخوره میره توی اکسپشن
             )
 
             try:
@@ -89,7 +87,7 @@ class UserClient:
         self.token = None
 
     def _request(self, method: str, endpoint: str, jsonData: dict = None, params: dict = None) -> dict:
-        Link = f"{self.baseLink}{endpoint}"
+        link  = f"{self.baseLink}{endpoint}"
         headers = {"Content-Type": "application/json"}
 
         if self.token:
@@ -98,11 +96,11 @@ class UserClient:
         try:
             response = requests.request(
                 method=method.upper(),
-                url=Link,
+                url=link ,
                 headers=headers,
                 json=jsonData,
                 params=params,
-                timeout=10
+                timeout=15 #این توی خود کتابخونه ریکوست هستش -----> اگه به مشکل بخوره میره توی اکسپشن
             )
 
             try:
@@ -197,18 +195,240 @@ class UserClient:
         return self._request("GET", f"/accounts/{account_id}/statement")
 
 
-# ===================== Menu =====================
-
-def AdminMenu(client: AdminClient):
-    pass
+# ============================================
 
 def UserMenu(client: UserClient):
-    pass
+    while True:
+        print("="*15+"User Menu"+"="*21)
+        print("1. Get Branches")
+        print("2. Create Account Request")
+        print("3. View My Account Requests")
+        print("4. Deposit")
+        print("5. Withdraw")
+        print("6. Card to Card Transfer")
+        print("7. Paya Transfer")
+        print("8. View My Accounts")
+        print("9. Get Account Balance")
+        print("10.Get Account Statement")
+        print("11.Logout")
+        print("0. Back")
+        print("="*50)
+
+        req = input("Enter your request: ").strip()
+
+        if req == "1":
+            print(client.get_branches())
+
+        elif req == "2":
+            try:
+                branch_id = int(input("Branch ID: "))
+                print(client.create_account_request(branch_id))
+            except ValueError:
+                print("Please enter a valid number.")
+
+        elif req == "3":
+            print(client.get_account_requests())
+
+        elif req == "4":
+            try:
+                acc_id = int(input("Account ID: "))
+                amount = float(input("Amount: "))
+                password = input("Password: ")
+                print(client.deposit(acc_id, amount, password))
+            except ValueError:
+                print("Invalid input.")
+
+        elif req == "5":
+            try:
+                acc_id = int(input("Account ID: "))
+                amount = float(input("Amount: "))
+                password = input("Password: ")
+                print(client.withdraw(acc_id, amount, password))
+            except ValueError:
+                print("Invalid input.")
+
+        elif req == "6":
+            from_card = input("From Card: ")
+            to_card = input("To Card: ")
+            amount = float(input("Amount: "))
+            password = input("Password: ")
+            print(client.Transfer(from_card, to_card, amount, password))
+
+        elif req == "7":
+            try:
+                from_acc = int(input("From Account ID: "))
+                to_iban = input("To IBAN: ")
+                amount = float(input("Amount: "))
+                password = input("Password: ")
+                print(client.paya_transfer(from_acc, to_iban, amount, password))
+            except ValueError:
+                print("Invalid input.")
+
+        elif req == "8":
+            print(client.get_my_accounts())
+
+        elif req == "9":
+            try:
+                acc_id = int(input("Account ID: "))
+                password = input("Password: ")
+                print(client.get_balance(acc_id, password))
+            except ValueError:
+                print("Invalid input.")
+
+        elif req == "10":
+            try:
+                acc_id = int(input("Account ID: "))
+                print(client.get_account_statement(acc_id))
+            except ValueError:
+                print("Invalid input.")
+
+        elif req == "11":
+            print(client.logout())
+            print("Logged out...")
+            break
+        elif req == "0":
+            break
+        else:
+            print("Invalid!!")
+
+#==========================================================
+
+def admin_menu(client: AdminClient):
+    while True:
+        print("="*15+"Admin Menu"+"="*21)
+        print("1. Create New Branch")
+        print("2. View All Branches")
+        print("3. View All Accounts")
+        print("4. View Account Requests")
+        print("5. Accepting Account Request")
+        print("6. Reject Account Request")
+        print("7. View Paya Requests")
+        print("8. Accepting Paya Request")
+        print("9. Reject Paya Request")
+        print("10.Logout")
+        print("0. Back")
+        print("="*50)
+
+        req = input("Enter your request: ").strip()
+        if req == "1":
+            name = input("Branch Name: ")
+            address = input("Branch Address: ")
+            print(client.create_branch(name, address))
+
+        elif req == "2":
+            print(client.get_branches())
+
+        elif req == "3":
+            print(client.get_all_accounts())
+
+        elif req == "4":
+            branch = input("Branch ID : ").strip()     # اگه خالی بزاره کلشو میده
+            if branch:
+                branch_id = int(branch)
+            else:
+                branch_id = None
+            print(client.get_account_requests(branch_id))
+
+        elif req == "5":
+            try:
+                req_id = int(input("Request ID: "))
+                print(client.approve_account_request(req_id))
+            except ValueError:
+                print("Please enter a valid number.")
+        elif req == "6":
+            try:
+                req_id = int(input("Request ID: "))
+                print(client.reject_account_request(req_id))
+            except ValueError:
+                print("Please enter a valid number.")
+        
+        elif req == "7":
+            print(client.paya_request())
+
+        elif req == "8":
+            try:
+                paya_id = int(input("Paya ID: "))
+                print(client.accept_paya(paya_id))
+            except ValueError:
+                print("Please enter a valid number.")
+
+        elif req == "9":
+            try:
+                paya_id = int(input("Paya ID: "))
+                print(client.reject_paya(paya_id))
+            except ValueError:
+                print("Please enter a valid number.")
+
+        elif req == "10":
+            print("Logged out...")
+            break
+
+        elif req == "0":
+            break
+        else:
+            print("Invalid!!")
+
+
+#=========================================================
 
 def main():
     Admin_Client = AdminClient()
     User_Client = UserClient()
-    pass
+    while True:
+        print("="*15+"Banking System"+"="*21)
+        print("1. User")
+        print("2. Admin")
+        print("0. Exit")
+        print("="*50)
+        Req = input("Enter to system: ").strip()
+        if Req == "1":
+            while True:
+                print("\n--- User Section ---")
+                print("1. Signup")
+                print("2. Login")
+                print("0. Back")
+                sub = input("Enter : ").strip()
+                if sub == "1":
+                    data = {
+                        "national_id": input("National ID: "),
+                        "name": input("Name: "),
+                        "family": input("Family: "),
+                        "birth_date": input("Birth Date (YYYY-MM-DD): "),
+                        "username": input("Username: "),
+                        "password": input("Password: ")
+                    }
+                    print(User_Client.signup(**data))
+                elif sub == "2":
+                    username = input("Username: ")
+                    password = input("Password: ")
+                    result = User_Client.login(username, password)
+                    if result.get("ok"):
+                        print("Login successful. Entering User Menu...") 
+                        #after login
+                        UserMenu(User_Client)
+                        break
+                    else:
+                        print("Login failed. \n Your Username or Password might be wrong, please try again !!")
+                elif sub == "0":
+                    break
+
+        elif Req == "2":
+            username = input("Admin Username: ")
+            password = input("Admin Password: ")
+            result = Admin_Client.login(username, password)
+            if result.get("ok"):
+                print("Admin login successful. Entering Admin Menu...")
+                #after login
+                admin_menu(Admin_Client)
+            else:
+                print("Login failed!!!")
+
+        elif Req == "0":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid!!")
+
 
 if __name__ == "__main__":
     main()
